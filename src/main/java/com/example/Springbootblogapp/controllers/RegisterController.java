2173c2a6e02a0a769c80a1c5c584c41063384baf
MikaValjakka <mika.valjakka@hotmail.com>
2023-06-14 18:43:27 +0300

    Login and REgister pages added.

---
+++ src/main/java/com/example/Springbootblogapp/controllers/RegisterController.java
@@ -0,0 +1,31 @@
+package com.example.Springbootblogapp.controllers;
+
+import com.example.Springbootblogapp.models.Account;
+import com.example.Springbootblogapp.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        Account newAccount = new Account();
+        model.addAttribute("account", newAccount);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute Account newAccount){
+        accountService.saveAccount(newAccount);
+
+        return "redirect:/";
+    }
+}
